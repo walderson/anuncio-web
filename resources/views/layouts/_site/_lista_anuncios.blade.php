ad40cfd2a90f928c7fa662bd7d4085f5637eeb80
Walderson Shimokawa <lord.caos@uol.com.br>
2021-03-29 11:42:11 -0400

    Listagem dos anúncios cadastrados utilizando cards no protótipo.

---
+++ resources/views/layouts/_site/_lista_anuncios.blade.php
@@ -0,0 +1,135 @@
+<div class="row section">
+    <h3 style="text-align: center">Anúncios</h3>
+    <div class="divider"></div>
+</div>
+
+<div class="row section">
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+                <a href="#"><img src="{{ asset('img/anuncio.jpg') }}"></a>
+                <span class="card-title">Título do anúncio</span>
+            </div>
+            <div class="card-content">
+                <p><b class="deep-orange-text darken-1">VENDA</b></p>
+                <p>Descrição do anúncio.</p>
+                <p>R$ 100.000,00</p>
+            </div>
+            <div class="card-action">
+                <a href="#">Mais detalhes...</a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+                <a href="#"><img src="{{ asset('img/anuncio.jpg') }}"></a>
+                <span class="card-title">Título do anúncio</span>
+            </div>
+            <div class="card-content">
+                <p><b class="deep-orange-text darken-1">VENDA</b></p>
+                <p>Descrição do anúncio.</p>
+                <p>R$ 100.000,00</p>
+            </div>
+            <div class="card-action">
+                <a href="#">Mais detalhes...</a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+                <a href="#"><img src="{{ asset('img/anuncio.jpg') }}"></a>
+                <span class="card-title">Título do anúncio</span>
+            </div>
+            <div class="card-content">
+                <p><b class="deep-orange-text darken-1">VENDA</b></p>
+                <p>Descrição do anúncio.</p>
+                <p>R$ 100.000,00</p>
+            </div>
+            <div class="card-action">
+                <a href="#">Mais detalhes...</a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+                <a href="#"><img src="{{ asset('img/anuncio.jpg') }}"></a>
+                <span class="card-title">Título do anúncio</span>
+            </div>
+            <div class="card-content">
+                <p><b class="deep-orange-text darken-1">VENDA</b></p>
+                <p>Descrição do anúncio.</p>
+                <p>R$ 100.000,00</p>
+            </div>
+            <div class="card-action">
+                <a href="#">Mais detalhes...</a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+                <a href="#"><img src="{{ asset('img/anuncio.jpg') }}"></a>
+                <span class="card-title">Título do anúncio</span>
+            </div>
+            <div class="card-content">
+                <p><b class="deep-orange-text darken-1">VENDA</b></p>
+                <p>Descrição do anúncio.</p>
+                <p>R$ 100.000,00</p>
+            </div>
+            <div class="card-action">
+                <a href="#">Mais detalhes...</a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+                <a href="#"><img src="{{ asset('img/anuncio.jpg') }}"></a>
+                <span class="card-title">Título do anúncio</span>
+            </div>
+            <div class="card-content">
+                <p><b class="deep-orange-text darken-1">VENDA</b></p>
+                <p>Descrição do anúncio.</p>
+                <p>R$ 100.000,00</p>
+            </div>
+            <div class="card-action">
+                <a href="#">Mais detalhes...</a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+                <a href="#"><img src="{{ asset('img/anuncio.jpg') }}"></a>
+                <span class="card-title">Título do anúncio</span>
+            </div>
+            <div class="card-content">
+                <p><b class="deep-orange-text darken-1">VENDA</b></p>
+                <p>Descrição do anúncio.</p>
+                <p>R$ 100.000,00</p>
+            </div>
+            <div class="card-action">
+                <a href="#">Mais detalhes...</a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+                <a href="#"><img src="{{ asset('img/anuncio.jpg') }}"></a>
+                <span class="card-title">Título do anúncio</span>
+            </div>
+            <div class="card-content">
+                <p><b class="deep-orange-text darken-1">VENDA</b></p>
+                <p>Descrição do anúncio.</p>
+                <p>R$ 100.000,00</p>
+            </div>
+            <div class="card-action">
+                <a href="#">Mais detalhes...</a>
+            </div>
+        </div>
+    </div>
+  </div>
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -3,20 +3,6 @@
 @section('content')
 @include('layouts._site._slider')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('Página inicial') }}
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('layouts._site._lista_anuncios')
 </div>
 @endsection
