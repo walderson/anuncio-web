0085ba74c23af13922e201579eb469a6b784d955
Walderson Shimokawa <lord.caos@uol.com.br>
2021-03-29 00:18:19 -0400

    Criação da página inicial do site e menu de navegação com Materialize.

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -21,10 +21,12 @@
         <nav>
             <div class="nav-wrapper blue">
                 <div class="container">
-                    <a href="#!" class="brand-logo">Logo</a>
+                    <a href="#!" class="brand-logo">AnúncioWeb</a>
                     <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                     <ul class="right hide-on-med-and-down">
                         <li><a href="#">Início</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Contato</a></li>
                     </ul>
                 </div>
             </div>
@@ -32,6 +34,8 @@
 
         <ul class="sidenav" id="mobile-demo">
             <li><a href="#">Início</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Contato</a></li>
         </ul>
 
         <main class="py-4">
