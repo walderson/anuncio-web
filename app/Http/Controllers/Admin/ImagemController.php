f3e67bd72ad1fe3645d9d287df98b98fe7d56b9e
Walderson Shimokawa <lord.caos@uol.com.br>
2021-05-24 13:45:48 -0400

    Implementação do cadastro de múltiplas imagens de um anúncio.

--- app/Http/Controllers/Admin/ImagemController.php
+++ app/Http/Controllers/Admin/ImagemController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 use App\Models\Anuncio;
+use App\Models\Imagem;
 
 class ImagemController extends Controller
 {
@@ -16,11 +18,10 @@ class ImagemController extends Controller
         return view('admin.imagens.index', compact('anuncio', 'imagens'));
     }
 
-    public function cadastrar()
+    public function cadastrar($id)
     {
-        $categorias = Categoria::all()->sortBy('titulo');
-        $municipios = Municipio::all()->sortBy('nome');
-        return view('admin.anuncios.cadastrar', compact(['categorias', 'municipios']));
+        $anuncio = Anuncio::find($id);
+        return view('admin.imagens.cadastrar', compact('anuncio'));
     }
 
     private function preencher(Request $request, $anuncio, $dados) {
@@ -52,17 +53,39 @@ class ImagemController extends Controller
         }
     }
 
-    public function salvar(Request $request)
+    public function salvar(Request $request, $id)
     {
         $dados = $request->all();
 
-        $anuncio = new Anuncio();
-        $this->preencher($request, $anuncio, $dados);
-        $anuncio->save();
+        $anuncio = Anuncio::find($id);
+        if ($anuncio->imagens()->count()) {
+            $imagem = $anuncio->imagens()->orderBy('ordem', 'desc')->first();
+            $ordem = isset($imagem->ordem) ? $imagem->ordem : 0;
+        } else {
+            $ordem = 0;
+        }
+
+        if ($request->hasFile('imagem')) {
+            $arquivos = $request->file('imagem');
+            foreach ($arquivos as $arquivo) {
+                $imagem = new Imagem();
+                $imagem->anuncio_id = $anuncio->id;
+                $imagem->ordem = ++$ordem;
+
+                $rand = rand(10000, 99999);
+                $dir = 'img/anuncios/' . Str::slug($anuncio->titulo, '_') . '/';
+                $ext = $arquivo->guessClientExtension();
+                $nomeArquivo = '_img_' . $rand . '.' . $ext;
+                $arquivo->move($dir, $nomeArquivo);
+                $imagem->imagem = $dir . $nomeArquivo;
+    
+                $imagem->save();
+            }
+        }
 
         $request->session()->flash('mensagem',
-            ['msg'=>'Anúncio cadastrado com sucesso!', 'class'=>'green white-text']);
-        return redirect()->route('admin.anuncios');
+            ['msg'=>'Imagens cadastradas com sucesso!', 'class'=>'green white-text']);
+        return redirect()->route('admin.imagens', $anuncio->id);
     }
 
     public function alterar($id)
--- resources/views/admin/imagens/cadastrar.blade.php
+++ resources/views/admin/imagens/cadastrar.blade.php
@@ -2,23 +2,24 @@
 
 @section('content')
     <div class="container">
-        <h2 class="center">Cadastrar Novo Anúncio</h2>
+        <h2 class="center">Cadastrar Novas Imagens</h2>
         <div class="row">
             <nav>
                 <div class="nav-wrapper blue darken-1">
                     <div class="col s12">
                         <a href="{{ route('admin.home') }}" class="breadcrumb">Início</a>
                         <a href="{{ route('admin.anuncios') }}" class="breadcrumb">Listagem de Anúncios</a>
-                        <a class="breadcrumb">Cadastrar Novo Anúncio</a>
+                        <a href="{{ route('admin.imagens', $anuncio->id) }}" class="breadcrumb">Imagens do Anúncio</a>
+                        <a class="breadcrumb">Cadastrar Novas Imagens</a>
                     </div>
                 </div>
             </nav>
         </div>
         <div class="row">
-            <form action="{{ route('admin.anuncios') }}" method="post"
+            <form action="{{ route('admin.imagens', $anuncio->id) }}" method="post"
                   enctype="multipart/form-data">
                 @csrf
-                @include('admin.anuncios._form')
+                @include('admin.imagens._form')
                 <button class="btn blue">Cadastrar</button>
             </form>
         </div>
