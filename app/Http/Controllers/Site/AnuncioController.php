8e7113dc6cdc2a2e3609491bce407ce0d992408b
Walderson Shimokawa <lord.caos@uol.com.br>
2021-05-25 15:14:59 -0400

    Exibindo os detalhes de um anúncio.

--- app/Http/Controllers/Site/AnuncioController.php
+++ app/Http/Controllers/Site/AnuncioController.php
@@ -5,9 +5,14 @@ namespace App\Http\Controllers\Site;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Anuncio;
+
 class AnuncioController extends Controller
 {
-    public function index() {
-        return view('site.anuncio');
+    public function index($id) {
+        $anuncio = Anuncio::find($id);
+        $imagens = $anuncio->imagens()->orderBy('ordem')->get();
+        $alinhamentos = ['center-align', 'left-align','right-align'];
+        return view('site.anuncio', compact('anuncio', 'imagens', 'alinhamentos'));
     }
 }
--- resources/views/site/anuncio.blade.php
+++ resources/views/site/anuncio.blade.php
@@ -8,37 +8,19 @@
     </div>
     <div class="row section">
         <div class="col s12 m8">
+            @if ($anuncio->imagens()->count())
             <div class="row">
                 <div class="slider">
                     <ul class="slides">
+                        @foreach ($imagens as $imagem)
                         <li>
-                            <img src="{{ asset('img/detalhe_1.jpg') }}">
-                            <div class="caption center-align">
-                                <h3>Título da imagem</h3>
-                                <h5 class="light grey-text text-lighten-3">Descrição da imagem</h5>
+                            <img src="{{ asset($imagem->imagem) }}">
+                            <div class="caption {{ $alinhamentos[rand(0, 2)] }}">
+                                <h3>{{ $imagem->titulo }}</h3>
+                                <h5 class="light grey-text text-lighten-3">{{ $imagem->descricao }}</h5>
                             </div>
                         </li>
-                        <li>
-                            <img src="{{ asset('img/detalhe_2.jpg') }}">
-                            <div class="caption right-align">
-                                <h3>Título da imagem</h3>
-                                <h5 class="light grey-text text-lighten-3">Descrição da imagem</h5>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="{{ asset('img/detalhe_3.jpg') }}">
-                            <div class="caption center-align">
-                                <h3>Título da imagem</h3>
-                                <h5 class="light grey-text text-lighten-3">Descrição da imagem</h5>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="{{ asset('img/detalhe_4.jpg') }}">
-                            <div class="caption left-align">
-                                <h3>Título da imagem</h3>
-                                <h5 class="light grey-text text-lighten-3">Descrição da imagem</h5>
-                            </div>
-                        </li>
+                        @endforeach
                     </ul>
                 </div>
             </div>
@@ -46,18 +28,29 @@
                 <button class="btn blue" onclick="sliderPrev()">Anterior</button>
                 <button class="btn blue" onclick="sliderNext()">Próximo</button>
             </div>
+            @else
+            <img src="{{ asset($anuncio->imagem) }}" class="responsive-img">
+            @endif
         </div>
         <div class="col s12 m4">
-            <h4>Título do anúncio</h4>
-            <blockquote>Descrição anúncio.</blockquote>
-            <p><b>Código:</b> 1731</p>
-            <p><b>Finalidade:</b> Venda</p>
-            <p><b>Categoria:</b> Automóvel</p>
-            <p><b>Endereço:</b> Rua das baneiras, 1650, Quitanda</p>
-            <p><b>CEP:</b> 12.434-865</p>
-            <p><b>Cidade:</b> Cuiabá - MT</p>
-            <p><b>Valor:</b> R$ 100.000,00</p>
+            <h4>{{ $anuncio->titulo }}</h4>
+            <blockquote>{{ $anuncio->descricao }}</blockquote>
+            <p><b>Código:</b> {{ $anuncio->id }}</p>
+            <p><b>Finalidade:</b> {{ $anuncio->finalidade }}</p>
+            <p><b>Categoria:</b> {{ $anuncio->categoria->titulo }}</p>
+            <p><b>Endereço:</b> {{ $anuncio->endereco }}</p>
+            <p><b>CEP:</b> {{ $anuncio->cep }}</p>
+            <p><b>Município:</b> {{ $anuncio->municipio->nome }} - {{ $anuncio->municipio->sigla_uf }}</p>
+            <p><b>Valor:</b> R$ {{ number_format($anuncio->valor, 2, ',', '.') }}</p>
             <a href="{{ route('site.contato') }}" class="btn deep-orange darken-1">Entrar em contato</a>
+        </div>
+    </div>
+    <div class="row section">
+        <div class="col s12 m8">
+            <div class="video-container">{!! $anuncio->mapa !!}</div>
+        </div>
+        <div class="col s12 m4">
+            {{ $anuncio->detalhes }}
         </div>
     </div>
 </div>
